f7c664b5e741df675aa2812f905a4af857b60593
Guizmohackett <46965883+gdelacroix@users.noreply.github.com>
2026-05-19 09:35:47 +0200

    commentaires rajoutés

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
- fetch("margot-robbie.json")
-    .then((actor) =>actor.json())
-    .then((data)=>{
-        console.log(data);
-        createActor(data);
+ fetch("margot-robbie.json")// on charge le json
+    .then((actor) => // une fois que le fichier est chargé
+        actor.json())// on le convertit en objet json
+    .then((data)=>{ // une fois que le json est converti en objet
+        console.log(data);// on affiche le résultat dans la console
+        createActor(data); // on appelle la fonction createActor en lui passant les données du json
     })
 
     function createActor(_data){
+        // on récupère les éléments du html
         const actorName=document.getElementsByClassName("actor-name")[0];
         const actorPicture=document.getElementsByClassName("actor-picture")[0];
         const actorMovies=document.getElementsByClassName("actor-movies")[0];
 
+        // on remplit les éléments du html avec les données du json
         actorName.innerHTML=_data.firstname + " " + _data.lastname;
         actorPicture.src=_data.picture;
 
         let moviesList="";
-
+    // on parcourt la liste des films de l'acteur et on les ajoute à la variable moviesList
         for (let x=0; x<_data.movies.length;x++){
             moviesList += "<li>" + _data.movies[x].name + "|" + _data.movies[x].year + "</li>";
         }
+        // on remplit l'élément actorMovies avec la liste des films de l'acteur
         actorMovies.innerHTML=moviesList;
 
     }
